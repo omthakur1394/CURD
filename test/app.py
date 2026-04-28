53069aad6a0847cd11b468a2bbcd2fdae3c334de
Om Thakur <131476626+omthakur1394@users.noreply.github.com>
2026-04-28 10:04:08 +0530

    new file

--- test/app.py
+++ test/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, url_for
+from flasssk import Flask, request, redirect, render_template, url_for
 
 # In-memory data store (resets when the app restarts).
 transactions = [
